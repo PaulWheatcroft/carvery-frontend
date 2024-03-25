dcaa80c58d4687256512f4691d51bd3b49019e4b
Paul Wheatcroft <paul_wheatcroft@hotmail.com>
2024-03-25 19:31:45 +0000

    Profile infor now returned from the API

--- src/components/Auth/AuthAccount.js
+++ src/components/Auth/AuthAccount.js
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
-import { fetchUserProfile } from "../../services/backendServices";
+import {
+  fetchUserProfile,
+  addUserProfile,
+} from "../../services/backendServices";
 
 const AuthAccount = () => {
   const { user, isAuthenticated, getAccessTokenSilently } = useAuth0();
@@ -13,16 +16,30 @@ const AuthAccount = () => {
       try {
         const token = await getAccessTokenSilently();
         setAccessToken(token);
-
+        console.log("from Auth0", user);
         const userIdString = user?.sub;
-        if (userIdString) {
+        if (userIdString && userIdString.includes("|")) {
           const userId = userIdString.split("|")[1];
           const handleFetchData = async () => {
             try {
               const data = await fetchUserProfile(userId, accessToken);
               console.log(data);
-              setUserData(data);
-              // Update state or perform other actions based on the fetched data
+              if (data) {
+                setUserData(data);
+                // Update state or perform other actions based on the fetched data
+              } else {
+                const userData = {
+                  id: userId,
+                  name: user?.given_name,
+                  email: user?.email,
+                };
+                const data = await addUserProfile(userData, accessToken);
+                console.log(data);
+                if (data) {
+                  setUserData(data);
+                  // Update state or perform other actions based on the fetched data
+                }
+              }
             } catch (error) {
               console.error(error);
             }
@@ -47,10 +64,10 @@ const AuthAccount = () => {
     <div>
       {userData && (
         <ul>
-          <li>Name: {userData.user.id}</li>
-          <li>Name: {userData.user.name}</li>
-          <li>Email: {userData.user.email}</li>
-          <li>Created date: {userData.user.created}</li>
+          <li>Name: {userData.user?.id}</li>
+          <li>Name: {userData.user?.name}</li>
+          <li>Email: {userData.user?.email}</li>
+          <li>Created date: {userData.user?.created}</li>
         </ul>
       )}
     </div>
--- src/services/backendServices.js
+++ src/services/backendServices.js
@@ -13,4 +13,22 @@ const fetchUserProfile = async (userId = null, accessToken) => {
   }
 };
 
-export { fetchUserProfile };
+const addUserProfile = async (userData, accessToken) => {
+  try {
+    const response = await fetch(`http://localhost:5000/add-user`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userData),
+    });
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
+
+export { fetchUserProfile, addUserProfile };
