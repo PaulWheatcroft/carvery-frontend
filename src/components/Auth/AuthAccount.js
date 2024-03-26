b0c4da826e2fce5206a5c6836fbf3805a5eb39fa
Paul Wheatcroft <paul_wheatcroft@hotmail.com>
2024-03-26 17:27:01 +0000

    Configured log in and profile fetch

--- src/components/Auth/AuthAccount.js
+++ src/components/Auth/AuthAccount.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-
 import {
   fetchUserProfile,
   addUserProfile,
@@ -10,61 +9,67 @@ const AuthAccount = () => {
   const { user, isAuthenticated, getAccessTokenSilently } = useAuth0();
   const [accessToken, setAccessToken] = useState(null);
   const [userData, setUserData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    const fetchUserId = async () => {
+    const fetchUserData = async () => {
       try {
         const token = await getAccessTokenSilently();
         setAccessToken(token);
-        console.log("from Auth0", user);
-        const userIdString = user?.sub;
-        if (userIdString && userIdString.includes("|")) {
-          const userId = userIdString.split("|")[1];
-          const handleFetchData = async () => {
-            try {
-              const data = await fetchUserProfile(userId, accessToken);
-              console.log(data);
-              if (data) {
-                setUserData(data);
-                // Update state or perform other actions based on the fetched data
-              } else {
-                const userData = {
-                  id: userId,
-                  name: user?.given_name,
-                  email: user?.email,
-                };
-                const data = await addUserProfile(userData, accessToken);
-                console.log(data);
-                if (data) {
-                  setUserData(data);
-                  // Update state or perform other actions based on the fetched data
-                }
-              }
-            } catch (error) {
-              console.error(error);
-            }
+
+        const userId = user?.sub?.split("|")[1];
+        if (!userId) {
+          setError("User ID not found");
+          return;
+        }
+
+        const userProfile = await fetchUserProfile(userId, accessToken);
+        console.log("userProfile", userProfile, userId);
+        if (userProfile === 404) {
+          const userData = {
+            id: userId,
+            name: user?.given_name,
+            email: user?.email,
           };
-          handleFetchData();
+          console.log("userData", userData);
+          const createdUser = await addUserProfile(userData, accessToken);
+          if (createdUser) {
+            const userProfile = await fetchUserProfile(userId, accessToken);
+            setUserData(userProfile);
+          }
+        } else {
+          setUserData(userProfile);
         }
       } catch (error) {
-        console.error(error);
+        setError(error.message);
+      } finally {
+        setLoading(false);
       }
     };
 
     if (isAuthenticated) {
-      fetchUserId();
+      fetchUserData();
     }
-  }, [isAuthenticated, getAccessTokenSilently, user?.sub, accessToken]);
+  }, [isAuthenticated, getAccessTokenSilently, user?.sub, accessToken, user]);
 
   if (!isAuthenticated) {
     return null; // Render nothing if not authenticated
+  }
+
+  if (loading) {
+    return <div>Loading...</div>; // Render loading state
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>; // Render error state
   }
 
   return (
     <div>
       {userData && (
         <ul>
-          <li>Name: {userData.user?.id}</li>
+          <li>ID: {userData.user?.id}</li>
           <li>Name: {userData.user?.name}</li>
           <li>Email: {userData.user?.email}</li>
           <li>Created date: {userData.user?.created}</li>
--- src/services/backendServices.js
+++ src/services/backendServices.js
@@ -1,16 +1,19 @@
 const fetchUserProfile = async (userId = null, accessToken) => {
-  try {
-    const response = await fetch(`http://localhost:5000/users/${userId}`, {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    });
+  const response = await fetch(`http://localhost:5000/users/${userId}`, {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  if (response.status === 404) {
+    return response.status;
+  } else if (response.status === 200) {
     const data = await response.json();
     return data;
-  } catch (error) {
-    console.error(error);
-    return null;
   }
+
+  // Handle any other error cases or throw an error
+  throw new Error("Failed to fetch user profile");
 };
 
 const addUserProfile = async (userData, accessToken) => {
